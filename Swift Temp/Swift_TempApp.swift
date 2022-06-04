18ca3423423e13ea56e03e448f683bf93f75fd1a
Ahmed <ahmed.ezzeldin.ibrahim@gmail.com>
2022-06-04 14:50:52 +0200

    Initial Commit

---
+++ Swift Temp/Swift_TempApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_TempApp.swift
+//  Swift Temp
+//
+//  Created by Codemaker on 04/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Swift_TempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
